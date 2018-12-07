08cf8bc07f974efa96cc0bdc755cac95d32ac38c
Peter Essman <pessman.dev@gmail.com>
2018-12-07 14:57:14 -0500

    updating print statements to use logging and log files

--- item_id/item_id.py
+++ item_id/item_id.py
@@ -1,9 +1,13 @@
 import requests
 import json
+import logging
 
 from mysql import connector
 
 import constants
+
+logging.basicConfig(filename='logs/item_id.log', level=logging.DEBUG,
+                    format='%(asctime)s -- %(levelname)s: %(message)s')
 
 '''
 create db connection based on values defined in constants file
@@ -19,8 +23,8 @@ db = connector.connect(
 '''
 debugging statements
 '''
-print(db)
-print(db.database)
+logging.debug(db)
+logging.debug(db.database)
 
 cursor = db.cursor(buffered=True)
 cursor_update = db.cursor(buffered=True)
@@ -38,11 +42,11 @@ for item_id_local, jan_id, mfc_id in cursor:
     if mfc_id is already defined, skip
     '''
     if mfc_id is not None:
-        print("skipping item_id_local {}".format(item_id_local))
+        logging.info("skipping item_id_local {}... already exists... {}".format(item_id_local, mfc_id))
         continue
 
     url = '{}{}'.format(constants.ITEM_DB_URL, jan_id)
-    print(url)
+    logging.debug(url)
 
     r = requests.get(url)
 
@@ -51,7 +55,7 @@ for item_id_local, jan_id, mfc_id in cursor:
     api may be returning 200 status for all calls
     '''
     if r.status_code != 200:
-        print(r.text)
+        logging.info(r.text)
         continue
 
     data = json.loads(r.text)
@@ -62,7 +66,7 @@ for item_id_local, jan_id, mfc_id in cursor:
     if 'items' in data and 'item' in data['items'] and 'id' in data['items']['item']:
         item_id = data['items']['item']['id']
     else:
-        print('no item id returned for {}'.format(item_id_local))
+        logging.info('no item id returned for {}'.format(item_id_local))
         continue
 
     '''
